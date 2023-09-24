1eda560c27024189c5ae4f8cc6d6029f5b8cd995
Victor <victor.santos.cd@gmail.com>
2023-09-23 23:36:11 -0300

    (main.py): Apply working logic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from csv import DictReader
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, FieldValidationInfo, field_validator
 import logging
 
 
@@ -30,27 +30,50 @@ class DadoCSV(BaseModel):
     Item: str
     Calories: int
     CaloriesFromFat: int
-    TotalFat_g: float
-    SaturatedFat_g: float
-    TransFat_g: float
-    Cholesterol_mg: int
-    Sodium_mg: int
-    Carbs_g: float
-    Fiber_g: float
-    Sugars_g: float
-    Protein_g: float
-    WeightWatchersPnts: int
+    # TODO fix the keys name
+    # TotalFat_g: float
+    # SaturatedFat_g: float
+    # TransFat_g: float
+    # Cholesterol_mg: int
+    # Sodium_mg: int
+    # Carbs_g: float
+    # Fiber_g: float
+    # Sugars_g: float
+    # Protein_g: float
+    WeightWatchersPnts: float
 
-    @validator("TotalFat_g", pre=True)
-    def validate_total_fat(cls, value):
-        if "TotalFat(g)" in cls.__annotations__:
-            """This if is necessary because has (g) in the name of the field"""
-            return value
-        raise ValueError("Field 'TotalFat(g)' is required.")
+    @field_validator("Company")
+    @classmethod
+    def validate_company(cls, value: str, info: FieldValidationInfo) -> str:
+        logging.info(info.config.get("title"))
+        return value
+
+    @field_validator("Item")
+    @classmethod
+    def validate_item(cls, value: str, info: FieldValidationInfo) -> str:
+        logging.info(info.config.get("title"))
+        return value
+
+    @field_validator("Calories")
+    @classmethod
+    def validate_calories(cls, value: int, info: FieldValidationInfo) -> int:
+        logging.info(info.config.get("title"))
+        return value
+
+    @field_validator("CaloriesFromFat")
+    @classmethod
+    def validate_caloriesfromfat(cls, value: int, info: FieldValidationInfo) -> int:
+        logging.info(info.config.get("title"))
+        return value
+
+    @field_validator("WeightWatchersPnts")
+    @classmethod
+    def validate_weightwatcherspnts(
+        cls, value: float, info: FieldValidationInfo
+    ) -> float:
+        logging.info(info.config.get("title"))
+        return value
 
 
 if __name__ == "__main__":
     fast_food_values = transform_csv_to_dictionary("FastFoodNutritionMenu")
-
-# TODO
-# Migrate to Pydantic V2 Style
