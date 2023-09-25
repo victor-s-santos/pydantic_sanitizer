76de17c371a30339f527892a91abb6e7cb502bcd
Victor <victor.santos.cd@gmail.com>
2023-09-25 12:34:41 -0300

    (main.py): Apply validators to each numeric field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from csv import DictReader
-from pydantic import BaseModel, conint
+from typing import Any
+from pydantic import BaseModel, conint, confloat, field_validator, FieldValidationInfo
 import logging
 
 
@@ -23,23 +24,125 @@ def transform_csv_to_dictionary(file_name: str) -> list:
         logging.error(f"Can't get list from given csv file name: {e}")
 
 
+def validate_null_in_number_field(value: Any) -> Any:
+    if not value:
+        return 0
+    return value
+
+
 class DadoCSV(BaseModel):
     """Realize the validation of each specific data format"""
 
     Company: str
     Item: str
-    Calories: conint(gt=0)
-    CaloriesFromFat: conint(gt=0)
-    TotalFat: float | conint(gt=0)
-    SaturatedFat: float | conint(gt=0)
-    TransFat: float | conint(gt=0)
-    Cholesterol: conint(gt=0)
-    Sodium: conint(gt=0)
-    Carbs: float | conint(gt=0)
-    Fiber: float | conint(gt=0)
-    Sugars: float | conint(gt=0)
-    Protein: float | conint(gt=0)
-    WeightWatchersPnts: float | str | None
+    Calories: conint(ge=0) | None
+    CaloriesFromFat: conint(ge=0) | None
+    TotalFat: confloat(ge=0) | conint(ge=0) | None
+    SaturatedFat: confloat(ge=0) | conint(ge=0) | None
+    TransFat: confloat(ge=0) | conint(ge=0) | None
+    Cholesterol: conint(ge=0) | None
+    Sodium: conint(ge=0) | None
+    Carbs: confloat(ge=0) | conint(ge=0) | None
+    Fiber: confloat(ge=0) | conint(ge=0) | None
+    Sugars: confloat(ge=0) | conint(ge=0) | None
+    Protein: confloat(ge=0) | conint(ge=0) | None
+    WeightWatchersPnts: confloat(ge=0) | str | None
+
+    @field_validator("Calories")
+    @classmethod
+    def validate_calories(
+        cls, value: int | None, info: FieldValidationInfo
+    ) -> int | None:
+        logging.info(info.config.get("title"))
+        return validate_null_in_number_field(value)
+
+    @field_validator("CaloriesFromFat")
+    @classmethod
+    def validate_caloriesfromfat(
+        cls, value: int | None, info: FieldValidationInfo
+    ) -> int | None:
+        logging.info(info.config.get("title"))
+        return validate_null_in_number_field(value)
+
+    @field_validator("TotalFat")
+    @classmethod
+    def validate_totalfat(
+        cls, value: float | int | None, info: FieldValidationInfo
+    ) -> float | int | None:
+        logging.info(info.config.get("title"))
+        return validate_null_in_number_field(value)
+
+    @field_validator("SaturatedFat")
+    @classmethod
+    def validate_saturedfat(
+        cls, value: float | int | None, info: FieldValidationInfo
+    ) -> float | int | None:
+        logging.info(info.config.get("title"))
+        return validate_null_in_number_field(value)
+
+    @field_validator("TransFat")
+    @classmethod
+    def validate_transfat(
+        cls, value: float | int | None, info: FieldValidationInfo
+    ) -> float | int | None:
+        logging.info(info.config.get("title"))
+        return validate_null_in_number_field(value)
+
+    @field_validator("Cholesterol")
+    @classmethod
+    def validate_cholesterol(
+        cls, value: int | None, info: FieldValidationInfo
+    ) -> int | None:
+        logging.info(info.config.get("title"))
+        return validate_null_in_number_field(value)
+
+    @field_validator("Sodium")
+    @classmethod
+    def validate_sodium(
+        cls, value: int | None, info: FieldValidationInfo
+    ) -> int | None:
+        logging.info(info.config.get("title"))
+        return validate_null_in_number_field(value)
+
+    @field_validator("Carbs")
+    @classmethod
+    def validate_carbs(
+        cls, value: float | int | None, info: FieldValidationInfo
+    ) -> float | int | None:
+        logging.info(info.config.get("title"))
+        return validate_null_in_number_field(value)
+
+    @field_validator("Fiber")
+    @classmethod
+    def validate_fiber(
+        cls, value: float | int | None, info: FieldValidationInfo
+    ) -> float | int | None:
+        logging.info(info.config.get("title"))
+        return validate_null_in_number_field(value)
+
+    @field_validator("Sugars")
+    @classmethod
+    def validate_sugars(
+        cls, value: float | int | None, info: FieldValidationInfo
+    ) -> float | int | None:
+        logging.info(info.config.get("title"))
+        return validate_null_in_number_field(value)
+
+    @field_validator("Protein")
+    @classmethod
+    def validate_protein(
+        cls, value: float | int | None, info: FieldValidationInfo
+    ) -> float | int | None:
+        logging.info(info.config.get("title"))
+        return validate_null_in_number_field(value)
+
+    @field_validator("WeightWatchersPnts")
+    @classmethod
+    def validate_weightwatcherspnts(
+        cls, value: float | int | None, info: FieldValidationInfo
+    ) -> float | int | None:
+        logging.info(info.config.get("title"))
+        return validate_null_in_number_field(value)
 
 
 if __name__ == "__main__":
