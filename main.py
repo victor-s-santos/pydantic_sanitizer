c14868896812716ac70af46772f50c8d86a15516
Victor <victor.santos.cd@gmail.com>
2023-09-26 20:45:03 -0300

    (main.py): refactor some code and create the write_csv_from_valid_data function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from csv import DictReader
+from csv import DictReader, DictWriter
+from models.data_models import DadoCSV
 import logging
 
 
@@ -22,5 +23,53 @@ def transform_csv_to_dictionary(file_name: str) -> list:
         logging.error(f"Can't get list from given csv file name: {e}")
 
 
+def write_csv_from_valid_data(
+    list_trust_data: list, output_filename: str = "output_files/cleaned_data.csv"
+) -> str:
+    """Export the csv from given valid data
+
+    Args:
+        list_trust_data (list): The list of trust_data
+
+    Returns:
+        str: The success message
+    """
+
+    colunm_names = [
+        "Company",
+        "Item",
+        "Calories",
+        "CaloriesFromFat",
+        "TotalFat",
+        "SaturatedFat",
+        "TransFat",
+        "Cholesterol",
+        "Sodium",
+        "Carbs",
+        "Fiber",
+        "Sugars",
+        "Protein",
+        "WeightWatchersPnts",
+    ]
+    with open(output_filename, "w") as file:
+        csv_file = DictWriter(file, fieldnames=colunm_names)
+
+        for data in list_trust_data:
+            csv_file.writerow(dict(data))
+
+    return f"The file has been exported in {output_filename} destination."
+
+
 if __name__ == "__main__":
     fast_food_values = transform_csv_to_dictionary("FastFoodNutritionMenu")
+
+    list_trust_data = []
+    list_trash_data = []
+
+    for value in fast_food_values:
+        try:
+            cleaned_data = DadoCSV(**value)
+            list_trust_data.append(cleaned_data.model_dump())
+
+        except Exception:
+            list_trash_data.append({"Valor com problema": value})
